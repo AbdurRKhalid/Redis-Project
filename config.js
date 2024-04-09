dd2011784d484eb006fc2e1001685b12a23f0ac0
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2024-04-09 19:05:39 +0200

    Added config.js properly to ignore.

--- config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    dev: {
-        db_password: "LkDZSXvQokTlTAj6Imi0HXPJzpCmPRNr",
-        db_host: "redis-12326.c300.eu-central-1-1.ec2.cloud.redislabs.com",
-        db_port: "12326"
-    }
-}
-
-export { config };
