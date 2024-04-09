91ba18ed9072149a87eba7a43b0e69bf3b8a5202
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2024-04-09 22:59:48 +0200

    Saved the book in the Redis Database.

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -1,10 +1,24 @@
 'use server'
 
-// import { client } from "@/lib/db"
+import { client } from "@/lib/db"
 import { redirect } from 'next/navigation'
 
 export async function createBook(formData) {
   const {title, rating, author, blurb} = Object.fromEntries(formData)
 
-  
+  // Creating a Random ID.
+  const id = Math.floor(Math.random() * 100000);
+
+  await client.connect();
+
+  // Saving new Hash in Redis.
+  await client.hSet(`book:${id}`, {
+    title,
+    rating,
+    author,
+    blurb
+  })
+
+  redirect('/');
+
 }
