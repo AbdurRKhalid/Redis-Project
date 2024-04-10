2be23ddff74197800dc8f87dc8535b7a4b8b8448
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2024-04-10 23:00:59 +0200

    Added the implementation of sorted set to make sure that we are adding book with unique title.

--- src/app/actions/create.js
+++ src/app/actions/create.js
@@ -11,13 +11,24 @@ export async function createBook(formData) {
 
   await client.connect();
 
+  // Adding the title to a sorted set to determine that we are adding books by unique title.
+  const unique = await client.zAdd('book', {
+    value: title,
+    score: id
+  }, {NX: true});
+
+  // Checking if the book has a unique title or not.
+  if(!unique) {
+    return {error: 'The Book with Same Title Already Exists!'};
+  }
+
   // Saving new Hash in Redis.
   await client.hSet(`book:${id}`, {
     title,
     rating,
     author,
     blurb
-  })
+  });
 
   redirect('/');
 
