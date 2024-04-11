9c3018bd0c04b7b2a44ad1c2bda6be28a0697fe7
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2024-04-11 21:42:42 +0200

    Created the Basic Pipeline to get All Books from Redis.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,16 @@
 import Link from 'next/link'
+import { client } from '@/lib/db'
 
 const getBooks = async () => {
-  
+  // getting scores of all sorted sets.
+  const result = await client.zRangeWithScores('books', 0, -1); // from start to end.
+
+  // creating pipelines to get each book based on score (id).
+  const books = await Promise.all(result.map((item) => {
+    return client.hGetAll(`books:${item.score}`);
+  }));
+
+  return books;
 }
 
 export default async function Home() {
